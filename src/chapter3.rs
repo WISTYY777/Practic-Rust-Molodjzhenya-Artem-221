8880a6e15113be23c578df76e24ae5bb1e18f10d
WISTYY777 <a.molodjzhenya@e-u.edu.ua>
2024-10-15 00:54:47 +0300

    Update chapter3.rs

--- src/chapter3.rs
+++ src/chapter3.rs
@@ -1,36 +1,34 @@
-//Глава 3
-#[test]//Тест 1
-fn main() {
-    let x: i32 = 5; 
-    let y: i32; 
+#[test] // Тест 1
+fn test1() {
+    let x: i32 = 5;
+    let _y: i32;
 
     assert_eq!(x, 5);
     println!("Success!");
 }
 
-#[test]//Тест 2
-fn main() {
+#[test] // Тест 2
+fn test2() {
     let mut x = 1;
     x += 2;
 
-    
     assert_eq!(x, 3);
     println!("Success!");
 }
 
-#[test]//Тест 3
-fn main() {
+#[test] // Тест 3
+fn test3() {
     let x: i32 = 10;
     let y: i32 = 20;
     {
         let y: i32 = 5;
         println!("The value of x is {} and value of y is {}", x, y);
     }
-    println!("The value of x is {} and value of y is {}", x, y); 
+    println!("The value of x is {} and value of y is {}", x, y);
 }
 
-#[test]//Тест 4
-fn main() {
+#[test] // Тест 4
+fn test4() {
     let x = define_x();
     println!("{}, world", x);
 }
@@ -39,19 +37,9 @@ fn define_x() -> String {
     let x = "hello".to_string();
     x
 }
-//другий варіант вирішення
-fn main() {
-    let x = define_x();
-    println!("{:?}, world", x);
-}
 
-fn define_x() -> &'static str {
-    let x = "hello";
-    x
-}
-
-#[test]//Тест 5
-fn main() {
+#[test] // Тест 5
+fn test5() {
     let x: i32 = 5;
     {
         let x = 12;
@@ -61,27 +49,27 @@ fn main() {
     assert_eq!(x, 5);
 
     let x = 42;
-    println!("{}", x); // Prints "42".
+    println!("{}", x);
 }
 
- #[test]//Тест 6
-fn main() {
-    let mut x: i32 = 1;
-    x = 7;
-    let x = x;
-    let y = 4;
-    let y = "I can also be bound to text!"; 
+#[test] // Тест 6
+fn test6() {
+    let mut _x: i32 = 1;
+    _x = 7;
+    let _x = _x;
+    let _y = 4;
+    let _y = "I can also be bound to text!";
 
     println!("Success!");
 }
 
-#[test]//Тест 7 
-fn main() {
+#[test] // Тест 7
+fn test7() {
     let _x = 1;
 }
 
- #[test]//Тест 8
-fn main() {
+#[test] // Тест 8
+fn test8() {
     let (mut x, y) = (1, 2);
     x += 2;
 
@@ -89,8 +77,8 @@ fn main() {
     assert_eq!(y, 2);
 }
 
- #[test]//Тест 9
-fn main() {
+#[test] // Тест 9
+fn test9() {
     let (x, y);
     (x, ..) = (3, 4);
     [.., y] = [1, 2];
