76a424811be589f3a158d4a0c2100b087e8b2e84
WISTYY777 <a.molodjzhenya@e-u.edu.ua>
2024-10-26 14:57:17 +0300

    Update Practice 9.rs

--- Practice 9.rs
+++ Practice 9.rs
@@ -2,7 +2,7 @@ fn is_prime(n: u32) -> bool {
     if n < 2 {
         return false;
     }
-    for i in 2..=f64::from(n).sqrt() as u32 {
+    for i in 2..=((n as f64).sqrt() as u32) {
         if n % i == 0 {
             return false;
         }
