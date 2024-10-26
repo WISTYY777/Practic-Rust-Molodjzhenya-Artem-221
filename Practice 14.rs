d359c63da060324a2a66572338bbdf791de07933
WISTYY777 <a.molodjzhenya@e-u.edu.ua>
2024-10-26 15:23:02 +0300

    Update Practice 14.rs

--- Practice 14.rs
+++ Practice 14.rs
@@ -10,36 +10,29 @@ struct Rectangle {
     b: Point,
 }
 
-fn area_occupied(xs: &Vec<Rectangle>) -> i32 {
-    let mut area = 0;
-    let mut overlaps = vec![];
+fn area_occupied(rectangles: &[Rectangle]) -> i32 {
+    let mut total_area = 0;
 
-    for i in 0..xs.len() {
-        let r1 = &xs[i];
-        let width1 = (r1.b.x - r1.a.x).abs();
-        let height1 = (r1.b.y - r1.a.y).abs();
-        area += width1 * height1;
+    for i in 0..rectangles.len() {
+        let width = (rectangles[i].b.x - rectangles[i].a.x).abs();
+        let height = (rectangles[i].b.y - rectangles[i].a.y).abs();
+        total_area += width * height;
 
-        for j in i + 1..xs.len() {
-            let r2 = &xs[j];
-            if rectangles_overlap(r1, r2) {
-                overlaps.push(get_overlap_area(r1, r2));
+        for j in i + 1..rectangles.len() {
+            if rectangles_overlap(&rectangles[i], &rectangles[j]) {
+                total_area -= overlap_area(&rectangles[i], &rectangles[j]);
             }
         }
     }
 
-    for overlap in overlaps {
-        area -= overlap;
-    }
-
-    area
+    total_area
 }
 
 fn rectangles_overlap(r1: &Rectangle, r2: &Rectangle) -> bool {
     r1.a.x < r2.b.x && r1.b.x > r2.a.x && r1.a.y > r2.b.y && r1.b.y < r2.a.y
 }
 
-fn get_overlap_area(r1: &Rectangle, r2: &Rectangle) -> i32 {
+fn overlap_area(r1: &Rectangle, r2: &Rectangle) -> i32 {
     let x_overlap = (r1.b.x.min(r2.b.x) - r1.a.x.max(r2.a.x)).max(0);
     let y_overlap = (r1.a.y.min(r2.a.y) - r1.b.y.max(r2.b.y)).max(0);
     x_overlap * y_overlap
@@ -47,25 +40,15 @@ fn get_overlap_area(r1: &Rectangle, r2: &Rectangle) -> i32 {
 
 fn test_data() -> Vec<Rectangle> {
     vec![
-        Rectangle {
-            a: Point { x: 2, y: 9 },
-            b: Point { x: 5, y: 3 },
-        },
-        Rectangle {
-            a: Point { x: 1, y: 8 },
-            b: Point { x: 11, y: 6 },
-        },
-        Rectangle {
-            a: Point { x: 9, y: 10 },
-            b: Point { x: 13, y: 2 },
-        },
+        Rectangle { a: Point { x: 2, y: 9 }, b: Point { x: 5, y: 3 } },
+        Rectangle { a: Point { x: 1, y: 8 }, b: Point { x: 11, y: 6 } },
+        Rectangle { a: Point { x: 9, y: 10 }, b: Point { x: 13, y: 2 } },
     ]
 }
 
 fn main() {
-    let data = test_data();
-    let occupied = area_occupied(&data);
-    println!("Зайнята площа: {}", occupied);
+    let occupied = area_occupied(&test_data());
+    println!("Occupied area: {}", occupied);
 }
 
 #[cfg(test)]
@@ -74,8 +57,8 @@ mod tests {
 
     #[test]
     fn area_occupied_test() {
-        let data = test_data();
-        let occupied = area_occupied(&data);
+        let occupied = area_occupied(&test_data());
         assert_eq!(occupied, 60);
     }
 }
+
