2cd327dffe2e0fbd983957de805e3fdf38acd799
WISTYY777 <a.molodjzhenya@e-u.edu.ua>
2024-10-26 15:05:38 +0300

    Update Practice 6.rs

--- Practice 6.rs
+++ Practice 6.rs
@@ -1,14 +1,12 @@
 fn gcd(a: u32, b: u32) -> u32 {
     if b == 0 {
-        a
-    } else {
-        gcd(b, a % b)
+        return a;
     }
+    gcd(b, a % b)
 }
-
 fn main() {
     let num1 = 48;
     let num2 = 18;
     let result = gcd(num1, num2);
-    println!("Найбільший спільний дільник {} і {}: {}", num1, num2, result);
+    println!("Найбільший спільний дільник: {}", result);
 }
